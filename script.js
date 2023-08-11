e3f356f887d9d37b8a2821f01986e9056b7c0c37
Adib_Ahbab <adibalvi476@gmail.com>
2023-08-12 00:50:07 +0600

    scroll  animation added

--- script.js
+++ script.js
@@ -10,3 +10,23 @@ function openTab(tabname) {
     event.currentTarget.classList.add('active-link');
     document.getElementById(tabname).classList.add('active-tab');
 }
+// ............Gsap code............
+
+gsap.from(".container", {
+    y: 200,
+    rotation:360,
+    duration: 1,
+    // scrollTrigger: {
+    //     trigger:".container",
+    //     start: "top 10%",
+    //     end:"top 30%",
+    //     toggleActions:"restart reverse none none",
+    //     markers: {
+    //         startColor:"white",
+    //         endColor:"red",
+    //         fontSize:"30px",
+    //         indent:100
+    //     },
+        scrub:2
+    // }
+})
